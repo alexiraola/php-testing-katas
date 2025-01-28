a9a3f47c5b9fe24902f34d4077e51c3763c46a3e
Alex Iraola <alex.iraola@gmail.com>
2025-01-28 15:12:24 +0100

    Email value object

---
+++ kata-hexagonal/src/domain/valueObjects/Email.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Alex\KataHexagonal\domain\valueObjects;
+
+use InvalidArgumentException;
+
+class Email
+{
+    private function __construct(private string $email)
+    {
+    }
+
+    public static function create(string $email): Email
+    {
+        Email::ensureIsValidEmail($email);
+        return new Email($email);
+    }
+
+    private static function ensureIsValidEmail(string $email): void
+    {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new InvalidArgumentException("Invalid email address: $email");
+        }
+    }
+
+    public function toString(): string
+    {
+        return $this->email;
+    }
+}
---
+++ kata-hexagonal/tests/domain/valueObjects/EmailTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use Alex\KataHexagonal\domain\valueObjects\Email;
+use PHPUnit\Framework\TestCase;
+
+class EmailTest extends TestCase
+{
+    public function testCreatesAnEmailForAGivenAddressInACorrectFormat(): void
+    {
+        $email = Email::create("example@example.com");
+        $this->assertEquals("example@example.com", $email->toString());
+    }
+
+    public function testDoesNotAllowCreatingAnEmailForAGivenIncorrectlyFormattedAddress(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $email = Email::create("invalid");
+    }
+
+    public function testConsidersTwoEmailsWithTheSameAddressAsEqual(): void
+    {
+        $aEmail = Email::create("example@example.com");
+        $otherEmail = Email::create("example@example.com");
+
+        $this->assertEquals($aEmail, $otherEmail);
+    }
+
+    public function testDifferentiatesBetweenTwoEmailsWithDifferentAddress(): void
+    {
+        $aEmail = Email::create("example@example.com");
+        $otherEmail = Email::create("anotherexample@example.com");
+
+        $this->assertNotEquals($aEmail, $otherEmail);
+    }
+}
