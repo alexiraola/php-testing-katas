f7ca900e38f034f9c97ea159a74e3456e2ca871e
Alex Iraola <alex.iraola@gmail.com>
2025-01-29 15:07:16 +0100

    Id value object

---
+++ kata-hexagonal/src/domain/common/Uuid.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Alex\KataHexagonal\domain\common;
+
+class Uuid
+{
+    public static function generateUuidV4(): string
+    {
+        $data = random_bytes(16);
+        // Set the version to 0100 (UUIDv4)
+        $data[6] = chr((ord($data[6]) & 0x0f) | 0x40);
+        // Set the variant to 10xx
+        $data[8] = chr((ord($data[8]) & 0x3f) | 0x80);
+
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+    }
+}
---
+++ kata-hexagonal/src/domain/valueObjects/Id.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Alex\KataHexagonal\domain\valueObjects;
+
+use Alex\KataHexagonal\domain\common\Uuid;
+use InvalidArgumentException;
+
+class Id
+{
+    private function __construct(private string $id)
+    {
+    }
+
+    public function toString(): string
+    {
+        return $this->id;
+    }
+
+    public static function generateUniqueIdentifier(): Id
+    {
+        return new Id(Uuid::generateUuidV4());
+    }
+
+    public static function createFrom(string $id): Id
+    {
+        Id::ensureIsValidId($id);
+        return new Id($id);
+    }
+
+    private static function ensureIsValidId(string $id): void
+    {
+        $isValidId = preg_match('/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}/', $id);
+
+        if (!$isValidId) {
+            throw new InvalidArgumentException("Invalid Id format");
+        }
+    }
+}
